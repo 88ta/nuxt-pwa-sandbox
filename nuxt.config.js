3b1ef019b72ea91790e928a072ac766cf80ee896
88ta <macyahashun@gmail.com>
2018-12-04 12:43:42 +0900

    Apply pwa config

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,8 +40,13 @@ module.exports = {
   */
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
   ],
+  manifest: {
+    name: 'nuxt-sandbox',
+    lang: 'ja'
+  },
   /*
   ** Axios module configuration
   */
